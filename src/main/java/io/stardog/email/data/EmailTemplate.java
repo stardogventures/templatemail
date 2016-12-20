8f13437ba59c1cfec18c26db20eef7aeca45cb73
Ian White <ibwhite@gmail.com>
2016-12-20 13:26:49 -0500

    adding the correct implementation of @Nullable

--- src/main/java/io/stardog/email/data/EmailTemplate.java
+++ src/main/java/io/stardog/email/data/EmailTemplate.java
@@ -7,7 +7,7 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import com.google.auto.value.AutoValue;
-import com.sun.istack.internal.Nullable;
+import javax.annotation.Nullable;
 
 import java.io.StringReader;
 
