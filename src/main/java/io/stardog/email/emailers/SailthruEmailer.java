bd0cc27fb835a0f5d1b2a9c4ad377840afcbfc0f
Ian White <ibwhite@gmail.com>
2016-12-20 12:38:51 -0500

    adding addTemplate() to TemplateEmailer interface

--- src/main/java/io/stardog/email/emailers/SailthruEmailer.java
+++ src/main/java/io/stardog/email/emailers/SailthruEmailer.java
@@ -2,6 +2,7 @@ package io.stardog.email.emailers;
 
 import com.sailthru.client.SailthruClient;
 import com.sailthru.client.params.Send;
+import io.stardog.email.data.EmailTemplate;
 import io.stardog.email.interfaces.TemplateEmailer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,14 +18,19 @@ public class SailthruEmailer implements TemplateEmailer {
     private final static Logger LOGGER = LoggerFactory.getLogger(SailthruEmailer.class);
     protected final Map<String,Object> globalVars = new HashMap<>();
 
-    public void putGlobalVar(String key, Object val) {
-        globalVars.put(key, val);
-    }
-
     @Inject
     public SailthruEmailer(SailthruClient client) {
         super();
         this.client = client;
+    }
+
+    public void putGlobalVar(String key, Object val) {
+        globalVars.put(key, val);
+    }
+
+    @Override
+    public void addTemplate(EmailTemplate template) {
+        throw new UnsupportedOperationException("Templates should be added in the Sailthru platform");
     }
 
     @Override
--- src/main/java/io/stardog/email/interfaces/TemplateEmailer.java
+++ src/main/java/io/stardog/email/interfaces/TemplateEmailer.java
@@ -1,12 +1,14 @@
 package io.stardog.email.interfaces;
 
+import io.stardog.email.data.EmailTemplate;
+
 import java.util.Map;
 
 /**
  * An TemplateEmailer is capable of sending emails by the name of that template.
- *  Any specifics involving adding templates, etc, are specific to the name of that implementation.
  */
 public interface TemplateEmailer {
     public String sendTemplate(String templateName, String toEmail, String toName, Map<String, Object> vars);
     public void putGlobalVar(String key, Object val);
+    public void addTemplate(EmailTemplate template);
 }
